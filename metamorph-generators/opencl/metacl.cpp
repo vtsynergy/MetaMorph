2d88eaba7ca7b41ddc81dc5f11bcaf5b0bd2a57e
Paul Sathre <sath6220@cs.vt.edu>
2019-06-11 13:40:29 -0400

    Support NULL localSize via {0, 0, 0} block_size a_dim3

--- metamorph-generators/opencl/metacl.cpp
+++ metamorph-generators/opencl/metacl.cpp
@@ -462,6 +462,7 @@ void PrototypeHandler::run(const MatchFinder::MatchResult &Result) {
     hostProto += "cl_command_queue queue, size_t (*grid_size)[3], size_t (*block_size)[3], ";
     setArgs += "  cl_int retCode = CL_SUCCESS;\n";
     //Add pseudo auto-scaling safety code
+	setArgs += "  bool nullBlock = (block_size != NULL && (*block_size)[0] == 0 && (*block_size)[1] == 0 && (*block_size)[2] == 0);\n";
     setArgs += "  size_t grid[3];\n";
     if (work_group_size[3] == 0 && !singleWorkItem) {
       setArgs += "  size_t block[3] = METAMORPH_OCL_DEFAULT_BLOCK_3D;\n";
@@ -489,9 +490,9 @@ void PrototypeHandler::run(const MatchFinder::MatchResult &Result) {
       setArgs += "      return CL_INVALID_WORK_GROUP_SIZE;\n";
       setArgs += "    }\n";
     }
-    setArgs += "    grid[0] = (*grid_size)[0] * (*block_size)[0];\n";
-    setArgs += "    grid[1] = (*grid_size)[1] * (*block_size)[1];\n";
-    setArgs += "    grid[2] = (*block_size)[2];\n";
+    setArgs += "    grid[0] = (*grid_size)[0] * (nullBlock ? 1 : (*block_size)[0]);\n";
+    setArgs += "    grid[1] = (*grid_size)[1] * (nullBlock ? 1 : (*block_size)[1]);\n";
+    setArgs += "    grid[2] = (nullBlock ? : 1 (*block_size)[2]);\n";
     setArgs += "    block[0] = (*block_size)[0];\n";
     setArgs += "    block[1] = (*block_size)[1];\n";
     setArgs += "    block[2] = (*block_size)[2];\n";
@@ -547,7 +548,7 @@ void PrototypeHandler::run(const MatchFinder::MatchResult &Result) {
     int workDim = 3;
     /// \todo TODO expose and handle work offset
     std::string offset = "NULL";
-    std::string globalSize = "grid", localSize = "block";
+    std::string globalSize = "grid", localSize = "(nullBlock ? NULL : block)";
     /// \todo TODO expose and handle eventWaitLists and retEvents
     std::string eventWaitListSize = "0", eventWaitList = "NULL", retEvent = "event";
     //Add the launch
